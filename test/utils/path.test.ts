a11a9777faa258bb9a09e309f7f6bd082156a3c9
KaKa <kaka@kakawebsitedemo.com>
2021-12-15 18:24:15 +0800

    test: increase coverage

---
+++ test/utils/path.test.ts
@@ -0,0 +1,27 @@
+import t from 'tap'
+import { normalizePath } from '../../lib/utils/path'
+
+const cases = [
+  ['/example/:userId', '/example/{userId}'],
+  ['/example/:userId/:secretToken', '/example/{userId}/{secretToken}'],
+  ['/example/near/:lat-:lng/radius/:r', '/example/near/{lat}-{lng}/radius/{r}'],
+  ['/example/near/:lat_1-:lng_1/radius/:r_1', '/example/near/{lat_1}-{lng_1}/radius/{r_1}'],
+  ['/example/*', '/example/{wildcard}'],
+  ['/example/:file(^\\d+).png', '/example/{file}.png'],
+  ['/example/at/:hour(^\\d{2})h:minute(^\\d{2})m', '/example/at/{hour}h{minute}m'],
+  ['/example/at/(^\\d{2})h(^\\d{2})m', '/example/at/{regexp1}h{regexp2}m'],
+  ['/example/at/(^([0-9]{2})h$)-(^([0-9]{2})m$)', '/example/at/{regexp1}-{regexp2}'],
+  ['/name::verb', '/name:verb'],
+  ['/api/v1/postalcode-jp/:code(^[0-9]{7}$)', '/api/v1/postalcode-jp/{code}'],
+  ['/api/v1/postalcode-jp/(^[0-9]{7}$)', '/api/v1/postalcode-jp/{regexp1}'],
+  // it is rare case, but will exist when people mis-configure
+  ['example', '/example']
+]
+
+t.test('formatParamUrl', function (t) {
+  t.plan(cases.length)
+
+  for (const kase of cases) {
+    t.equal(normalizePath(kase[0]), kase[1])
+  }
+})
