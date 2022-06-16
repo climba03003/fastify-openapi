239191730f28882a14fdb349c4cc649ac8e234ed
KaKa <kaka@kakawebsitedemo.com>
2022-06-16 19:01:07 +0800

    fix: remove done in onRegister

--- lib/utils/fastify-hooks.ts
+++ lib/utils/fastify-hooks.ts
@@ -21,14 +21,13 @@ export function addHooks (fastify: FastifyInstance): void {
     done()
   })
 
-  fastify.addHook('onRegister', function (instance, _, done) {
+  fastify.addHook('onRegister', function (instance, _) {
     // since each instance may have their own $ref storage
     // we need it for each registration
     instance.addHook('onReady', function (done) {
       fastify[kDocumentGenerator].addSchemas(fastify.getSchemas())
       done()
     })
-    done()
   })
 
   /**
